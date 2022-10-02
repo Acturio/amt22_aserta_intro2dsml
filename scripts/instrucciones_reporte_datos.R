db942c99cde9a62ab4407aa196e77aaa137c0652
Acturio <act.arturo.b@ciencias.unam.mx>
2022-10-02 14:30:07 -0500

    minior fixes

---
+++ scripts/instrucciones_reporte_datos.R
@@ -0,0 +1,54 @@
+
+Análisis de variable de respuesta:
+
+Realiza un análisis estadístico y gráfico de la variables de respuesta
+Incluye valores estadísticos como:
+  Promedio
+  Mediana
+  Mínimo
+  Máximo
+  Desviación estándar
+  Intervalos de confianza
+Incluye en el análisis gráfico:
+  Histograma
+  Boxplot
+Agrega interpretación de resultados
+
+Crea una variable que calcule la antiguedad de la vivienda.
+
+
+
+
+Análisis univariado de variables explicativas:
+
+Realiza el análisis estadístico y gráfico de 10 variables categóricas o
+continuas que sospeches que puedan estar asociadas positiva o negativamente
+con la variable de respuesta.
+
+En el caso de usar valores continuos, estima los mismos valores estadísticos
+que en el punto 1 y agrega sus respectivas gráficas. Agrega interpretación de
+resultados
+
+En el caso de variables categóricas, agrega conteos, proporciones, desviación
+estándar de la media e intervalos de confianza de la proporción de cada categoría.
+Hint: Desviación estándar de media (Bernoulli) = sqrt( p(1-p) / n )
+
+Agrega gráficos de barras o intervalos de confianza alrededor de la media.
+
+
+
+Análisis multivariado de variables explicativas y respuesta
+
+Para cada una de las variables explicativas, agrega un análisis multivariado
+en donde se realice el análisis de la variable de respuesta por categoría
+(en el caso de las variables categóricas vs variable de respuesta)
+
+En el caso de variables continuas, agrega un diagrama de dispersión y calcula
+la correlación entre ambas variables.
+Hint: correlación = cor(x, y)
+Hint2: correlación = cor(dataset); donde dataset contiene variables continuas
+Interpreta resultados
+
+
+
+
--- scripts/script_10_visualización.R
+++ scripts/script_10_visualización.R
@@ -14,7 +14,7 @@ data("diamonds")
 glimpse(diamonds)
 
 for (j in 1:10) {
- diamonds[sample(35940, sample(20001, 16457)),j] <- NA
+ diamonds[sample(35940, 20000),j] <- NA
 }
 
 plot_intro(diamonds)
@@ -81,7 +81,7 @@ diamonds %>%
 
 diamonds %>% 
   ggplot( aes( x = cut)) + 
-  geom_bar( color= "darkblue", fill= "cyan", alpha= 0.7) +
+  geom_bar( color= "darkblue", fill= "#c4c8d4", alpha= 0.7) +
   scale_y_continuous(labels = scales::comma_format()) +
   ggtitle("Distribución de calidad de corte") +
   theme_dark()
@@ -89,37 +89,77 @@ diamonds %>%
 
 df_pie <- diamonds %>%
   group_by(cut) %>% 
-  summarise(freq = n(), .groups='drop')
+  summarise(
+   freq = n(), 
+#   media = mean(price, na.rm = T),
+#  sd = sd(price, na.rm = T),
+   .groups='drop'
+   )
+
+diamonds %>%
+  group_by(cut) %>% 
+  tally()
 
 
 df_pie %>% 
+  filter(!is.na(cut)) %>% 
   ggplot( aes( x = "", y=freq,  fill = factor(cut)))  +
   geom_bar(width = 1, stat = "identity")  +
   coord_polar(theta = "y", start=0)
 
-ggplot(data = diamonds) +
+diamonds %>% 
+  filter(!is.na(cut)) %>% 
+  ggplot() +
   geom_bar( mapping = aes(x = cut, fill = cut), show.legend = T, width = 1) +
   theme(aspect.ratio = 1) +
   labs(x= NULL, y = NULL) +
-  coord_polar() +
-  theme_classic()
+  coord_polar()
+
+#### Ejercicio: Crear gráfico de pie con el precio medio por categoría
+
+df_pie <- diamonds %>% 
+ group_by(clarity) %>% 
+ summarise(mean = mean(price, na.rm=T), .groups='drop') 
+
+df_pie %>% 
+ filter(!is.na(clarity)) %>% 
+ ggplot() + 
+ geom_bar(aes(x = "", y=mean, fill = clarity), 
+          show.legend = T, width = 1, stat = "identity") + 
+ theme(aspect.ratio = 1) + 
+ labs(x= NULL, y = NULL) + 
+ coord_polar("y", start =0) + 
+ theme_classic() 
+
+
+
+
+
+
 
 
 diamonds %>% 
+ filter(!is.na(clarity)) %>% 
   ggplot( aes( y = clarity)) + 
   geom_bar( color= "darkblue", fill= "black", alpha= 0.7) +
   geom_text(aes(label = scales::comma(..count..)), stat = "count", 
-            vjust = 1, hjust = 1.1,colour = "white") +
+            vjust = 0.5, hjust = 1, colour = "black") +
   scale_x_continuous(labels = scales::comma_format()) +
   ggtitle("Distribución claridad") +
-  theme_get()
+  theme_bw() +
+ coord_flip()
 
 diamonds %>% 
+ filter(!is.na(clarity)) %>% 
   ggplot( aes( y = clarity)) + 
   geom_bar( color= "darkblue", fill= "black", alpha= 0.7) +
   geom_text(aes(label = scales::percent(..count../sum(..count..) ) ), 
             stat = "count", vjust = -0.25, colour = "darkblue") +
-  scale_x_continuous(labels = scales::comma_format()) +
+  geom_text(aes(label = ..count.. ), 
+            stat = "count", vjust = 1, colour = "yellow") +
+  scale_x_continuous(
+   labels = scales::comma_format(), 
+   n.breaks = 15) +
   ggtitle("Distribución claridad") +
   coord_flip()
 
@@ -127,26 +167,33 @@ diamonds %>%
 #### Análisis multivariado ####
 
 diamonds %>% 
-  ggplot(aes(y= price, x = cut, color = cut))  + 
-  geom_jitter(size = 1.2, alpha = 0.5)
+ ggplot(aes(y= price, x = cut, color = cut))  + 
+ geom_jitter(size = 0.6, alpha = 0.3, height = 2) +
+ geom_boxplot(color = "black", alpha = 0.5)
 
 diamonds %>% 
   ggplot(aes(y = price, x = cut, color = cut))  + 
   geom_boxplot(size=1.2, alpha= 0.5)
 
 diamonds %>% 
-  ggplot(aes(x= price ,fill=cut))  + 
-  geom_histogram(position = 'identity', alpha = 0.5)
+  ggplot(aes(x= price, fill = cut))  + 
+  geom_histogram(position = 'identity', alpha = 0.5, color = "black")
 
 diamonds %>% 
+ filter(!is.na(price), !is.na(cut)) %>% 
   ggplot(aes(x = price, fill = cut))  + 
-  geom_histogram(position = 'identity', alpha = 0.5) +
-  facet_wrap(~ cut, ncol = 1)
+  geom_histogram(position = 'identity', alpha = 0.5, color = "black") +
+  facet_wrap(~ cut, ncol = 2, scales = "free")
 
 diamonds %>% 
   ggplot( aes(x = carat , y = price)) +
   geom_point(aes(col = clarity) ) +
   geom_smooth()
+
+
+
+
+
 
 diamonds %>% 
   ggplot( aes(x = carat, y = price)) +
